5aaca227fff5a5c929eb669722179f03839cc966
Shawn Lapuz <shangel.lapuz@gmail.com>
2023-02-02 17:58:35 +0800

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
